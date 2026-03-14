aa9498603229f2ba045dfb9d793bab5a64fef4cc
PranayRaj98 <135486017+PranayRaj98@users.noreply.github.com>
2026-03-15 00:12:34 +0530

    render deployment

--- app.py
+++ app.py
@@ -2,27 +2,12 @@ import gradio as gr
 from chatbot.rag_chain import ask_question
 
 def chat(message, history):
-    """
-    Chat function for the Gradio ChatInterface.
-    message: The current user message.
-    history: The conversation history (not used here, but available if ask_question supports it).
-    """
     return ask_question(message)
 
 demo = gr.ChatInterface(
     fn=chat,
-    title="🏢 UHC Insurance Policy Chatbot",
-    description="Ask questions about UnitedHealthcare policies. I'm here to help you understand your coverage and answer any questions you might have.",
-    # theme=gr.themes.Soft(),
-    examples=[
-        "Amvuttra (vutrisiran) is medically necessary for the treatment of what?",
-        "When is bariatric surgery required",
-        "When did India win the World Cup?",
-    ],
-    # retry_btn="🔄 Retry",
-    # undo_btn="↩️ Undo",
-    # clear_btn="🗑️ Clear",
+    title="UHC Insurance Policy Chatbot",
+    description="Ask questions about UnitedHealthcare policies"
 )
 
-if __name__ == "__main__":
-    demo.launch()
+demo.launch(server_name="0.0.0.0", server_port=7860)
